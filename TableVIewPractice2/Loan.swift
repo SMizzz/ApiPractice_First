26dcd6f4d7d59e3addd69ddd19243f488957801d
SMizzz <shinn.mizzz@gmail.com>
2021-06-21 20:26:53 +0900

    Add Codable

--- TableVIewPractice2/Loan.swift
+++ TableVIewPractice2/Loan.swift
@@ -7,10 +7,36 @@
 
 import Foundation
 
-struct Loan {
+struct Loan: Codable {
   var name: String = ""
   var country: String = ""
   var use: String = ""
   var amount: Int = 0
   var date: String = ""
+  
+  enum CodingKeys: String, CodingKey {
+    case name
+    case country = "location"
+    case use
+    case amount = "loan_amount"
+    case date = "posted_date"
+  }
+  
+  enum LocationKeys: String, CodingKey {
+    case country
+  }
+  
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    name = try values.decode(String.self, forKey: .name)
+    use = try values.decode(String.self, forKey: .use)
+    amount = try values.decode(Int.self, forKey: .amount)
+    date = try values.decode(String.self, forKey: .date)
+    let location = try values.nestedContainer(keyedBy: LocationKeys.self, forKey: .country)
+    country = try location.decode(String.self, forKey: .country)
+  }
+}
+
+struct LoanDataStore : Codable {
+  var loans: [Loan]
 }
--- TableVIewPractice2/MainViewController.swift
+++ TableVIewPractice2/MainViewController.swift
@@ -14,11 +14,11 @@ class MainViewController: UIViewController {
   
   let restaurantCell = "restaurantCell"
   
-  var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
-  
-  var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
-  
-  var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+//  var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+//
+//  var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+//
+//  var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
   
   var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
   
@@ -71,30 +71,43 @@ class MainViewController: UIViewController {
       
       // 성공했을 때(데이터파싱)
       do {
-        // data parsing
-        let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
-//        print(jsonResult)
-        let jsonLoans = jsonResult!["loans"] as! [AnyObject]
-        print(jsonLoans)
-        
-        for jsonLoan in jsonLoans {
-          var loan = Loan()
-          loan.name = jsonLoan["name"] as! String
-          loan.amount = jsonLoan["loan_amount"] as! Int
-          loan.date = jsonLoan["posted_date"] as! String
-          loan.use = jsonLoan["use"] as! String
-          let location = jsonLoan["location"] as! [String: AnyObject]
-          loan.country = location["country"] as! String
-          
-          self.loans.append(loan)
-        }
+        let decoder = JSONDecoder()
+        let loanDataStore = try decoder.decode(LoanDataStore.self, from: data!)
+        self.loans = loanDataStore.loans
         
         OperationQueue.main.addOperation {
           self.tableView.reloadData()
         }
+        
       } catch let error {
         print(error)
       }
+      
+//      do {
+//        // data parsing
+//        let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+////        print(jsonResult)
+//        let jsonLoans = jsonResult!["loans"] as! [AnyObject]
+//        print(jsonLoans)
+//
+//        for jsonLoan in jsonLoans {
+//          var loan = Loan()
+//          loan.name = jsonLoan["name"] as! String
+//          loan.amount = jsonLoan["loan_amount"] as! Int
+//          loan.date = jsonLoan["posted_date"] as! String
+//          loan.use = jsonLoan["use"] as! String
+//          let location = jsonLoan["location"] as! [String: AnyObject]
+//          loan.country = location["country"] as! String
+//
+//          self.loans.append(loan)
+//        }
+//
+//        OperationQueue.main.addOperation {
+//          self.tableView.reloadData()
+//        }
+//      } catch let error {
+//        print(error)
+//      }
     }.resume()
   }
   
